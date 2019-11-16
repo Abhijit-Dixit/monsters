d77d184467095d276abc0a93e989ca37ba91901b
Abhijit-Dixit <abhijitdixitent@gmail.com>
2019-11-16 11:09:52 +0530

    Updates

---
+++ precache-manifest.59fffb58dcc6a6c8f7eb0bdcac202658.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "915e39e682c3ff8f1fbac4d03f73cd6a",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "8d73882a870dae09657e",
+    "url": "/monsters/static/css/main.b4067e35.chunk.css"
+  },
+  {
+    "revision": "c6214f979b742b0bcadf",
+    "url": "/monsters/static/js/2.f65d83a6.chunk.js"
+  },
+  {
+    "revision": "8d73882a870dae09657e",
+    "url": "/monsters/static/js/main.b6099c55.chunk.js"
+  },
+  {
+    "revision": "852058801be7c79d6b02",
+    "url": "/monsters/static/js/runtime-main.98fd8d22.js"
+  }
+]);
